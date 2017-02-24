5033c50f85f6d14cb705d9e3c91cf90328e43db8
Pavel Salimov <pavel.salimov@booking.com>
2017-02-24 10:35:24 +0100

    Try

--- lib/HBase/Client/RegionScanner.pm
+++ lib/HBase/Client/RegionScanner.pm
@@ -18,10 +18,12 @@ sub new {
 
 sub next_async {
 
-    my ($self, $exclude_start) = @_;
+    my ($self) = @_;
 
-    my ($node, $scanner_id, $number_of_rows, $next_call_seq) = @$self{ qw ( node scanner_id number_of_rows next_call_seq ) };
-    my ($region, $scan) = defined $scanner_id ? () : @$self{ qw ( region scan ) };
+    my ($node, $scanner_id, $number_of_rows, $next_call_seq, $exclude_start) = @$self{ qw ( node scanner_id number_of_rows next_call_seq exclude_start ) };
+    my $first_call = defined $scanner_id;
+
+    my ($region, $scan) = $first_call ? () : @$self{ qw ( region scan ) };
 
     return $node->scan_async( $region, $scan, $scanner_id, $number_of_rows + !!$exclude_start, $next_call_seq)
         ->then( sub {
@@ -29,9 +31,9 @@ sub next_async {
                 my ($response) = @_;
 
                 $self->{next_call_seq} = $next_call_seq + 1;
-                $self->{scanner_id} = $response->get_scanner_id unless defined $scanner_id;
+                $self->{scanner_id} = $response->get_scanner_id if $first_call;
 
-                if ($exclude_start and my @results = @{$response->get_results_list}){
+                if ($first_call and $exclude_start and my @results = @{$response->get_results_list}){
                     if ($results[0]->get_cell(0)->get_row eq $scan->{start_row}){
                         shift @results;
                         $response->set_results_list( [@results] );
