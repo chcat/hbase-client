ffc7b9bf5a9937afebc42a9e65cca43161db0685
Pavel Salimov <pavel.salimov@booking.com>
2017-08-08 15:33:40 +0200

    Typo fix

--- lib/HBase/Client/TableScanner.pm
+++ lib/HBase/Client/TableScanner.pm
@@ -145,7 +145,7 @@ sub _region_scanner {
                 # 2) stop_row is given and we do normal scan and the region we gonna scan starts after the stop row or at it (cause stop_row is being excluded anyway)
                 # 3) stop_row is given and we do reverse scan and the region we gonna scan ends at of before the stop row
 
-                return undef if !region || ($stop_row && ($reversed && $region->end le $stop_row || !$reversed && $region->start ge $stop_row));
+                return undef if !$region || ($stop_row && ($reversed && $region->end le $stop_row || !$reversed && $region->start ge $stop_row));
 
                 return $region->scanner( $self->{scan}, $self->{number_of_rows} );
 
