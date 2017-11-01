c8cc22050b117e103a8119f7d3bbddf0d06e2665
Pavel Salimov <pavel.salimov@booking.com>
2017-11-01 08:59:16 +0100

    Clear error messages for zookeeper locator

--- lib/HBase/Client/ZookeeperMetaHolderLocator.pm
+++ lib/HBase/Client/ZookeeperMetaHolderLocator.pm
@@ -30,22 +30,24 @@ sub locate {
 
         my $node = Net::ZooKeeper->new( $self->{quorum} )->get( $self->{path} );
 
+        die "zookeeper unreachable or node does not exists" unless $node;
+
         my ( $magic, $id_length, $blob ) = unpack ( 'CNA*', $node );
 
-        die "Unexpected zookeeper @{[$self->{zookeeper_quorum}]} hbase node @{[$self->{zookeeper_path}]} content" unless $magic == 255;
+        die "unexpected zookeeper node content" unless $magic == 255;
 
         my $server_info = HBase::Client::Proto::MetaRegionServer->decode( substr $blob, length('PBUF') + $id_length );
 
         warn "Unexpected RPC version @{[$server_info->get_rpc_version]}" unless $server_info->get_rpc_version == 0;
 
-        die "Meta holder is not ready" unless $server_info->get_state() == 3;
+        die "meta holder is not ready" unless $server_info->get_state() == 3;
 
         my $server = $server_info->get_server;
 
         $deferred->resolve( $server->get_host_name .':'.$server->get_port );
 
         1;
-    } or do { $deferred->reject("Error locating meta holder via zookeeper @{[$self->{quorum}]}@{[$self->{path}]}: $!") };
+    } or do { $deferred->reject("Error locating meta holder via zookeeper @{[$self->{quorum}]} node @{[$self->{path}]}: $!") };
 
     return $deferred->promise;
 }
